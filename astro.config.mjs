db02944dae045a895bd7f74771ec3375f38cb9eb
Paulo Truta <ptruta@pm.me>
2024-04-17 21:21:10 +0200

    Fix missing comma

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
 	// Enable Svelte to support Svelte components.
 	integrations: [svelte()],
 	// Support a custom domain
-	site: 'https://edgebox.io'
+	site: 'https://edgebox.io',
 	redirects: {
     		'/sh': 'https://raw.githubusercontent.com/edgebox-iot/install/main/install.sh?token=GHSAT0AAAAAACOR7TIKNSRJG7AASMEKZYZAZRAD6AA'
   	}
