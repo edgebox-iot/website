43e7f8d3e4f9d75586b13afceb190f38c46918b2
Paulo Truta <ptruta@pm.me>
2024-04-17 21:54:58 +0200

    Removed setup for redirect install

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,5 @@ export default defineConfig({
 	// Enable Svelte to support Svelte components.
 	integrations: [svelte()],
 	// Support a custom domain
-	site: 'https://edgebox.io',
-	redirects: {
-  		'/sh': 'https://github.com/edgebox-iot/install/releases/download/v1.0.0-beta.1/install.sh'
-	}
+	site: 'https://edgebox.io'
 });
